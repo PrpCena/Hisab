7f13c40bbff28e7ed64756885fdc724ab90fd991
prparmar1 <161976176+PrpCena@users.noreply.github.com>
2026-01-14 04:33:21 -0500

    Initial commit

---
+++ src/main/java/com/prp/Hisab/HisabApplication.java
@@ -0,0 +1,13 @@
+package com.prp.Hisab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prp/Hisab/HisabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prp.Hisab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HisabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
