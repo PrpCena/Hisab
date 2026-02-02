15cfcffdd336f6b574393aaafafbd8ed3ea5506c
prparmar1 <161976176+PrpCena@users.noreply.github.com>
2026-02-02 06:51:28 -0500

    Add MethodArgumentNotValidException to global exception handling

--- src/main/java/com/prp/Hisab/controller/GlobalExceptionHandler.java
+++ src/main/java/com/prp/Hisab/controller/GlobalExceptionHandler.java
@@ -2,20 +2,50 @@ package com.prp.Hisab.controller;
 
 import com.prp.Hisab.domain.dto.ApiErrorResponse;
 import com.prp.Hisab.exception.HisabException;
+import com.prp.Hisab.exception.UserNotAuthenticatedException;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import java.time.Instant;
+import java.util.stream.Collectors;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    
-    @ExceptionHandler(UserNotAuthenticatedException.class)
-    public ResponseEntity<ApiErrorResponse> handleException(HisabException ex, HttpServletRequest request) {
-        ApiErrorResponse errorResponse = new ApiErrorResponse(ex.getStatus()
-                .value(), ex.getStatus()
-                .getReasonPhrase(), ex.getMessage(), request.getRequestURI(), Instant.now());
-        return new ResponseEntity<>(errorResponse, ex.getStatus());
-    }
+
+  @ExceptionHandler(UserNotAuthenticatedException.class)
+  public ResponseEntity<ApiErrorResponse> handleException(
+      HisabException ex, HttpServletRequest request) {
+    ApiErrorResponse errorResponse =
+        new ApiErrorResponse(
+            ex.getStatus().value(),
+            ex.getStatus().getReasonPhrase(),
+            ex.getMessage(),
+            request.getRequestURI(),
+            Instant.now());
+    return new ResponseEntity<>(errorResponse, ex.getStatus());
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ResponseEntity<ApiErrorResponse> handleValidationExceptions(
+      MethodArgumentNotValidException ex, HttpServletRequest request) {
+    String detailedMessages =
+        ex.getBindingResult().getFieldErrors().stream()
+            .map(error -> error.getField() + ": " + error.getDefaultMessage())
+            .collect(Collectors.joining(", "));
+
+    ApiErrorResponse errorResponse =
+        new ApiErrorResponse(
+            HttpStatus.BAD_REQUEST.value(),
+            HttpStatus.BAD_REQUEST.getReasonPhrase(),
+            "Validation Failed: " + detailedMessages,
+            request.getRequestURI(),
+            Instant.now());
+
+    return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+  }
 }
