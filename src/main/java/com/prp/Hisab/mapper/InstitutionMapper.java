e72821f96a4fff58995cf8d7a8cf3903967143a7
prparmar1 <161976176+PrpCena@users.noreply.github.com>
2026-02-08 13:38:14 -0500

    Update to reflect new changes to the entity relationship

--- src/main/java/com/prp/Hisab/mapper/InstitutionMapper.java
+++ src/main/java/com/prp/Hisab/mapper/InstitutionMapper.java
@@ -4,11 +4,9 @@ import com.prp.Hisab.config.GlobalMapperConfig;
 import com.prp.Hisab.domain.Institution;
 import com.prp.Hisab.domain.entity.InstitutionEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(config = GlobalMapperConfig.class)
 public interface InstitutionMapper {
-  @Mapping(target = "accounts", ignore = true)
   Institution toDomain(InstitutionEntity entity);
 
   InstitutionEntity toEntity(Institution domain);
